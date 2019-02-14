70dd2506f72dd21b139804067bdfd75581ebf9ef
MichaelRomero <liikemike@aim.com>
2019-02-14 18:14:43 -0500

    cleaned up seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,19 @@
 require_all 'app'
+# To load 1st Gen Pokemon (Kanto) 1 - 151
+# To load 2nd Gen Pokemon (Johto) 152 - 251
+# To load 3rd Gen Pokemon (Hoenn) 252 - 386
+# To load 4th Gen Pokemon (Sinnoh) 387 - 493
+# To load 5th Gen Pokemon (Unova) 494 - 649
+# To load 6th Gen Pokemon (Kalos) 650 - 721
+# To load 7th Gen Pokemon (Alola) 722 - 807
 
-# Loads 1st Gen Pokemon (Kanto)
 def load_pokemon
-  i = 1
+  dex_entry = 1
 
-  until i == 151 do
-    # dex_entry = rand(807)
+  until dex_entry == 151 do
     system "clear"
-    puts "Loading #{i}/150 Pokemon"
-    pokemon = fetch_api(i)
+    puts "Loading #{dex_entry}/150 Pokemon"
+    pokemon = fetch_api(dex_entry)
     name = pokemon["name"]
     base_exp = pokemon["base_experience"]
     type_one = pokemon["types"].find { |type| type["slot"] == 1 }["type"]["name"]
@@ -17,10 +22,10 @@ def load_pokemon
       name: name,
       element: type_one,
       base_experience: base_exp,
-      pokedex: i
+      pokedex: dex_entry
     )
 
-    i += 1
+    dex_entry += 1
   end
 end
 
